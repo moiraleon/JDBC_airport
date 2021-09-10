880e0921cd38fac7eca75ee89a97630cc69e5c12
Moira Leon <moira.leon2@gmail.com>
2021-09-10 12:09:25 -0600

    adding data types and Column annotations to Passenger, constructor with arguments, getters and setters and empty constructor

--- src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
+++ src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
@@ -14,5 +14,30 @@ public class Passenger {
     @Column(name = "ID")
     private int id;
 
+    @Column(name = "NAME")
+    private String name;
 
+    public Passenger(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public Passenger(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
