905913847a8022b9adbe8a3d48131c5a6e15232b
Moira Leon <moira.leon2@gmail.com>
2021-09-10 12:57:51 -0600

    adding Passenger List, get and add methods

--- src/main/java/com/pluralsight/hibernatefundamentals/airport/Airport.java
+++ src/main/java/com/pluralsight/hibernatefundamentals/airport/Airport.java
@@ -2,6 +2,9 @@ package com.pluralsight.hibernatefundamentals.airport;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name = "AIRPORTS")
@@ -14,6 +17,17 @@ public class Airport {
 
     @Column(name = "NAME")
     private String name;
+
+    @OneToMany(mappedBy = "airport")
+    private List<Passenger> passengers= new ArrayList<>();
+
+    public List<Passenger> getPassengers() {
+        return Collections.unmodifiableList(passengers);
+    }
+
+    public void addPassengers(Passenger passenger) {
+       passengers.add(passenger);
+    }
 
     public Airport(int id, String name) {
         this.id = id;
