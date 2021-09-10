1fbbbff098e35cb3d0253ba49b35f0e7f6843240
Moira Leon <moira.leon2@gmail.com>
2021-09-10 12:11:43 -0600

    adding data types, column annotation, getters and setters, construcor with arguments and empty constructor to Ticket

--- src/main/java/com/pluralsight/hibernatefundamentals/airport/Ticket.java
+++ src/main/java/com/pluralsight/hibernatefundamentals/airport/Ticket.java
@@ -13,4 +13,32 @@ public class Ticket {
     @Id
     @Column(name = "ID")
     private int id;
+
+    @Column(name = "NUMBER")
+    private String number;
+
+    public Ticket(int id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public Ticket(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
