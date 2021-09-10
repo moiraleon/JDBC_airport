6a5d7d3dca19861e091acd9ec2789bbb4ba5f203
Moira Leon <moira.leon2@gmail.com>
2021-09-10 11:56:12 -0600

    adding id datatype to passenger

--- src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
+++ src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
@@ -1,10 +1,17 @@
 package com.pluralsight.hibernatefundamentals.airport;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "PASSENGERS")
 public class Passenger {
+
+
+    @Id
+    @Column(name = "ID")
+    private int id;
 }
