9da12c7f0fd5e986e9b7fc86fc185302470887ec
Moira Leon <moira.leon2@gmail.com>
2021-09-10 11:54:15 -0600

    annotating passenger class

---
+++ src/main/java/com/pluralsight/hibernatefundamentals/airport/Passenger.java
@@ -0,0 +1,10 @@
+package com.pluralsight.hibernatefundamentals.airport;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PASSENGERS")
+public class Passenger {
+}
